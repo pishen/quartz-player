dadbeddfb8bb36f91031075b5a727c7159136602
pishen <pishen02@gmail.com>
2014-05-15 23:18:53 +0800

    remove submit js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-	$("#submit-job").click(function() {
-		var req = {
-			id: $("#job-id").val(), 
-			email: $("#email").val(),
-			cmd: $("#cmd").val(),
-			cron: $("#cron").val()
-		}
-		$.post("/add", JSON.stringify(req), function() {
-			window.location.reload()
-		})
-	})
+	
 })
